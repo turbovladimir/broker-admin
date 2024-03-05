4617f5b0b371c53764af9524838856d4254e105a
turbo <sk8.killer@mail.ru>
2024-03-05 15:55:00 +0530

    build upd

--- bin/build_prod.sh
+++ bin/build_prod.sh
@@ -3,3 +3,4 @@
 git reset --hard
 git pull
 cd docker && docker-compose -f compose_prod.yaml down && docker-compose -f compose_prod.yaml up -d
+docker exec -it c-broker-admin sh -c "composer dump-env prod && composer install"
