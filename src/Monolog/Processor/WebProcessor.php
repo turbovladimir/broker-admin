89c277409a656e6ce6e54ee20bb09beb6dd8e06a
turbo <sk8.killer@mail.ru>
2024-03-05 12:34:51 +0530

    add loan controller and views

---
+++ src/Monolog/Processor/WebProcessor.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Monolog\Processor;
+
+use Monolog\LogRecord;
+use Monolog\Processor\ProcessorInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class WebProcessor implements ProcessorInterface
+{
+    private RequestStack $requestStack;
+
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
+
+    public function __invoke(LogRecord $record)
+    {
+        $currentRequest = $this->requestStack->getCurrentRequest();
+
+        if ($currentRequest) {
+            $record['extra'] = [
+                'request' => [
+                    'ip' => $currentRequest->getClientIp(),
+                    'uri' => $currentRequest->getUri(),
+                    'method' => $currentRequest->getMethod(),
+                    'query' => $currentRequest->query->all(),
+                    'post_data' => $currentRequest->request->all()
+                ]
+            ];
+        }
+
+        return $record;
+    }
+}
