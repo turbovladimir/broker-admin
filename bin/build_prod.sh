bce65980eaf99b76c6be6a968e0260d1cd91236a
turbo <sk8.killer@mail.ru>
2024-03-05 15:28:51 +0530

    build upd

--- bin/build_prod.sh
+++ bin/build_prod.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
+git reset --hard
 git pull
 cd docker && docker-compose -f compose_prod.yaml down && docker-compose -f compose_prod.yaml up -d
-docker exec -it c-broker-admin sh -c "composer install"
