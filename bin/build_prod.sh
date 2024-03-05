71db439f82428101e3e005bbb0a035b22ba29577
turbo <sk8.killer@mail.ru>
2024-03-05 14:04:27 +0530

    build upd

--- bin/build_prod.sh
+++ bin/build_prod.sh
@@ -2,4 +2,4 @@
 
 git pull
 cd docker && docker-compose -f compose_prod.yaml down && docker-compose -f compose_prod.yaml up -d
-docker exec -it c-broker-admin sh -c "cd /app && composer install"
+docker exec -it c-broker-admin sh -c "composer install"
