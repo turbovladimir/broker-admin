7a8aea6b20604f5593972895bf80f7309fe6d700
turbo <sk8.killer@mail.ru>
2024-04-22 20:41:37 +0530

    sms sender log errors add

--- src/Service/Sms/Sender.php
+++ src/Service/Sms/Sender.php
@@ -4,36 +4,49 @@ namespace App\Service\Sms;
 
 use App\Service\Sms\DTO\SendSmsRequest;
 use GuzzleHttp\Exception\BadResponseException;
+use GuzzleHttp\Psr7\Request;
 use Psr\Http\Client\ClientInterface;
 use GuzzleHttp\RequestOptions;
+use Psr\Http\Message\ResponseInterface;
 use Psr\Log\LoggerInterface;
 
 class Sender
 {
 
-    public function __construct(private ClientInterface $client, private LoggerInterface $smsLogger, private array $credentals)
+    public function __construct(private ClientInterface $client, private LoggerInterface $telegramLogger, private array $credentals)
     {
     }
 
     public function send(SendSmsRequest $request): void
     {
         try {
-            $this->client->request($request->getMethod(), $request->getUrl(), [RequestOptions::FORM_PARAMS => [
+            $response = $this->client->request($request->getMethod(), $request->getUrl(), [RequestOptions::FORM_PARAMS => [
                 'action' => 'post_sms',
                 'message' => $request->getMessage(),
                 'target' => implode(',', $request->getPhoneNumbers()),
                 'user' => $this->credentals['user'],
                 'pass' => $this->credentals['password']
             ]]);
+
+            $this->validateResponse($response);
         } catch (BadResponseException $exception) {
             $response = $exception->getResponse();
-            $this->smsLogger->error('Send verify sms fail.', [
+            $this->telegramLogger->error('Send verify sms fail.', [
                 'request' => $request,
                 'response' => [
                     'status' => $response->getStatusCode(),
                     'body' => (string)$response->getBody()
                 ]
             ]);
+        }
+    }
+
+    private function validateResponse(ResponseInterface $response) : void
+    {
+        $body = (string)$response->getBody();
+
+        if (str_contains($body, 'errors')) {
+            throw new BadResponseException('Body ciontains errors', new Request('','', []), $response);
         }
     }
 }
