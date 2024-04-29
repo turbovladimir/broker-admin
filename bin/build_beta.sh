9b840481ede22dd13dc0296984e49d1209a0b3b0
turbo <sk8.killer@mail.ru>
2024-04-29 13:43:25 +0530

    add beta build config

--- bin/build_beta.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git reset --hard
-git pull
-cd docker && docker-compose -f compose_beta.yaml down && docker-compose -f compose_beta.yaml up -d
-docker exec -it c-broker-beta sh -c "composer dump-env beta && composer install"
